6ae1f1684b0676f32795bfb3f2638e6bf8e8116a
Edivaldo Borges <edborgesti@gmail.com>
2018-04-15 19:59:07 -0300

    Nova árvore AVL. 15/04/2018.

--- Documents/NetBeansProjects/ArvoreBinariaAVL/src/ExercicioAVL/PrincipalAVL.java
+++ Documents/NetBeansProjects/ArvoreBinariaAVL/src/ExercicioAVL/PrincipalAVL.java
@@ -24,7 +24,7 @@ public class PrincipalAVL {
         arvore.inserir(8);
         arvore.inserir(9);
         arvore.inserir(10);
-        arvore.inserir(12);
+        arvore.inserir(17);
 
         System.out.println("\n EM ORDEM");
         arvore.imprimeEmOrdem();
